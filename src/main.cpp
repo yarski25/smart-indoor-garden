41414713d6626392ad3ae7f03f272c477247436b
yarski25 <yaroslav.saprykin@gmail.com>
2023-04-13 22:54:34 +0200

    water level detection and pump control implemented for testing

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,14 @@
 
 #include <Arduino.h>
 
-//#define AOUT_PIN A0 // Arduino pin that connects to AOUT pin of moisture sensor
-//#define AIR_VALUE 530 // CMS value air
-//#define WATER_VALUE 230 // CMS value water
+#define WLS_PIN 25 // Arduino pin that connects to analog pin of water level sensor (WLS)
+#define WLS_LOW 0 // low level of WLS
+#define WLS_HIGH 2400 // high level of WLS
 
-//#define RELAY_PIN    2  // Arduino pin that connects to relay
+#define RELAY_PIN 12  // Arduino pin that connects to relay
+
+unsigned long myTime1;
+unsigned long myTime2;
 
 // CMS intervals (air, wet, water)
 // VERY wET <230;330>
@@ -27,15 +30,57 @@
 
 void setup() {
   Serial.begin(115200);
-  //pinMode(RELAY_PIN, OUTPUT);
-  //digitalWrite(RELAY_PIN, LOW);
-  //delay(5000);
+  Serial.print("system init...");
+
+  pinMode(WLS_PIN, INPUT);
+  delay(100);
+  //analogReadResolution(12);                   // Sets the sample bits and read resolution, default is 12-bit (0 - 4095), range is 9 - 12 bits
+  //delay(100);
+  //analogSetWidth(12);                         // Sets the sample bits and read resolution, default is 12-bit (0 - 4095), range is 9 - 12 bits
+  //delay(100);
+
+  pinMode(RELAY_PIN, OUTPUT);
+  delay(100);
+  digitalWrite(RELAY_PIN, HIGH);
+  delay(10000);
+  Serial.println("");
+  Serial.print("system READY...");
+  delay(1000);
+  Serial.println("");
 }
 
 void loop() {
 
-  Serial.print("test msg...");
-  delay(5000);
+  int waterLevel = analogRead(WLS_PIN);
+  delay(1000);
+  waterLevel = map(waterLevel, WLS_LOW, WLS_HIGH, 0, 100);
+
+  if(waterLevel > 90){
+    Serial.print("water level HIGH (");
+    Serial.print(waterLevel);
+    Serial.println("%)");
+    myTime1 = millis();
+    digitalWrite(RELAY_PIN, LOW);
+    Serial.println("water pump ON");
+    delay(1000);
+    digitalWrite(RELAY_PIN, HIGH);
+    Serial.println("water pump OFF");
+    myTime2 = millis();
+    Serial.print("water pump operated for ");
+    Serial.print((myTime2-myTime1));
+    Serial.println("ms");
+    delay(60000);
+  }
+  else{
+    Serial.print("water level LOW (");
+    Serial.print(waterLevel);
+    Serial.println("%)");
+    digitalWrite(RELAY_PIN, HIGH);
+    Serial.println("water pump OFF");
+  }
+
+  
+
 //  int value = analogRead(AOUT_PIN); // read the analog value from sensor
 //
 //  if(value > WATER_VALUE && value < (WATER_VALUE + intervals))
